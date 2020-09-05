d75e96d1eadfa9699d58a7e12c9ab63a113dca4e
Felipe França <contatofelipefranca12@gmail.com>
2020-09-04 22:43:26 -0300

    Exibi o nome da escola

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Etec Adolpho Berezin");
+        }
+    }
+}
